44c9655cee72acd14bce799d04f7ddc8b3c2add2
unknown <ye.mo@BJPD007109.zhaopin.com.cn>
2015-01-14 18:00:54 +0800

    comprehensive exercise.

--- src/main.lua
+++ src/main.lua
@@ -2,5 +2,9 @@ local lib = require("lib")
 
 --[[
   测试
-]]
-print("5!=", lib.factWithTail(5, 1))
+--]]
+print("5!=" .. lib.factWithTail(5, 1))
+
+print("please input a number:")
+a = io.read("*number")
+print("your input length:"..#tostring(a))
