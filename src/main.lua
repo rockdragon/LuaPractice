25eccb3788e49b88c5d04cedadc153247c8481dd
unknown <ye.mo@BJPD007109.zhaopin.com.cn>
2015-01-14 11:15:07 +0800

    my first code snippet.

--- src/main.lua
+++ src/main.lua
@@ -1,1 +1,22 @@
-print("Hello world")
+--阶乘
+function fact (n)
+  if n == 0 then
+    return 1
+  else
+    return n * fact(n-1)
+  end
+end
+
+--尾递归阶乘
+function factWithTail(n, m)
+  if n == 0 then
+    return m
+  else 
+    return factWithTail(n-1, n*m)
+  end
+end
+
+--[[
+  测试
+]]
+print("5!=", factWithTail(5, 1))
