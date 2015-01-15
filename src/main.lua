c61e8cb3d594bf7afc9c7106159ce2174f903bb5
Locke <moyerock@gmail.com>
2015-01-15 20:14:29 +0800

    iterator exercise.

--- src/main.lua
+++ src/main.lua
@@ -1,9 +1,8 @@
 local lib = require("lib")
 
 --[[
-  测试
---]]
 print("5!=" .. lib.factWithTail(5, 1))
+--]]
 
 --[[
 local days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"}
@@ -16,9 +15,6 @@ print("please input a number(1-7):")
 local i = io.read("*number")
 
 print(days[i % 7])
---]]
-
-
 
 local a, b = 1, 10
 if a < b then
@@ -27,3 +23,12 @@ if a < b then
   print(a)
 end
 print(a, b)
+--]]
+
+local numbers = {1, 2, 3, 4, 5, 6, 8}
+function iterate(list)
+  for k, v in ipairs(list) do
+    print(k.."="..v)
+  end
+end
+iterate(numbers)
