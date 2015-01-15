a22967d8a4b835d751f8d222a2269bf83ea7009d
Locke <moyerock@gmail.com>
2015-01-15 22:11:30 +0800

    table will be arrange in the numerical order when key be numeric.

--- src/main.lua
+++ src/main.lua
@@ -32,7 +32,7 @@ function iterateK(t)
 end
 
 local numbers = {
-  ["Monday"]=1, ["Tuesday"]=2, ["Wednesday"]=3, ["Thursday"]=4, ["Friday"]=5, ["Saturday"]=6, ["Sunday"]=7}
+  ["Monday"]=1, ["Tuesday"]=2, ["Wednesday"]=3, ["Thursday"]=4, ["Friday"]=5, ["Saturday"]=8, ["Sunday"]=7}
 function reverseTable(t)
   local rt = {}
   for k in pairs(t) do
