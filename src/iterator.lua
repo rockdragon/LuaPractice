4638cca48e34b4ac806ec1a80a167c30c18f586f
Locke <moyerock@gmail.com>
2015-01-17 17:51:52 +0800

    iterator exploration ver 2.

--- src/iterator.lua
+++ src/iterator.lua
@@ -5,12 +5,23 @@ module = {}
     1, iterator
     2, table
     3, position
+    
+    ==> in each of iteration, for-in uses the iterator as iterator(table, position)
+    ==>   itertaor is responsible for auto-incresing position
+    
   for-in output result:
     k, v
+
 --]]
-function module.traverse(list, init)
-  init = init or nil
-  return next, list, init
+function getnext(list, pos)
+  pos = pos or 0
+  pos = pos + 1
+  local v = list[pos]
+  if v then return pos, v else return nil end
+end
+
+function module.traverse(list)
+  return getnext, list, nil
 end
 
 return module
