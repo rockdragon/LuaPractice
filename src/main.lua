64c629a98c68cff03b9563808cbf03b1d137cdc6
Locke <moyerock@gmail.com>
2015-01-17 21:14:23 +0800

    ternary operand.

--- src/main.lua
+++ src/main.lua
@@ -1,10 +1,9 @@
 --loadstring & assert
 print("enter function to be plotted (with variable 'x'):")
 local l = io.read()
-local f = assert(loadstring("return ".. l))
+local f = load("local x=...; return " .. l)
 for i=1, 20 do
-  x = i
-  print(string.rep("*", f() or i))
+  print(string.rep("*", f(i) or i))
 end
 
   
