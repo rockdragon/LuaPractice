6e3f2547d30180e01bbe24fea3b47ee93e571055
Locke <moyerock@gmail.com>
2015-01-18 14:55:40 +0800

    pcall elegant invoking.

--- src/main.lua
+++ src/main.lua
@@ -8,13 +8,14 @@ for i=1, 20 do
 end
 --]]
 
-local file, msg
+local file, status
 repeat
   print "enter a file name:"
   local name = io.read()
   if not name then return end
-  file, msg = io.open(name, "r")
-  if not file then print(msg) end
+  --file = assert(io.open(name, "r"))
+  status, file = pcall(io.open, name, "r")
+  if not file then print(status) end
 until file
 
 if file then 
