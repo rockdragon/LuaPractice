196d3aa27daadaedc75db462b7391e5e025a30f8
unknown <ye.mo@BJPD007109.zhaopin.com.cn>
2015-01-19 17:48:57 +0800

    luasocket exercise.

---
+++ src/socket/win.lua
@@ -0,0 +1,25 @@
+local http = require("socket.http")
+
+ local response_body = {}
+ 
+local res, code, response_headers =  http.request{
+    method="GET",
+    url = "http://localhost/",
+    sink = ltn12.sink.table(response_body)
+  }
+
+if code == 200 then
+  for k, v in pairs(response_headers) do
+    print(k..":"..v)
+  end
+  
+  --body
+  for k, v in pairs(response_body) do
+    print(v)
+  end  
+end
+
+
+
+
+
