53e1436117bf6c6a2f780ae9933dbdb7c678ea27
unknown <ye.mo@BJPD007109.zhaopin.com.cn>
2015-01-20 17:33:52 +0800

    sample.

--- src/sample/win.lua
+++ src/sample/win.lua
@@ -1,12 +1,33 @@
-print("please input a sentence:")
-local line = io.read()
-local pos = 1
-while line do
-  local s, e = string.find(line, "%w+", pos)
-  if s then
-    pos = e + 1
-    print(string.sub(line, s, e))
+function allwords()
+  print("please input a sentence:")
+  local line = io.read()
+  local pos = 1
+  return function()
+    while line do
+      local s, e = string.find(line, "%w+", pos)
+      if s then
+        pos = e + 1
+        print(string.sub(line, s, e))
+      else
+        line = io.read()
+        pos = 1
+      end
+    end
+    return nil
+  end
+end
+
+function prefix(w1, w2)
+  return w1 .. " " .. w2
+end
+
+local statetab = {}
+
+function insert(index, value)
+  local list = statetab[index]
+  if list == nil then
+    statetab[index] = {value}
   else
-    break
+    list[#list + 1] = value
   end
 end
