8dfa5a92175afd0da74eb288c38cbcc0bc4f9010
unknown <ye.mo@BJPD007109.zhaopin.com.cn>
2015-01-20 17:49:08 +0800

    modulr sample for part 1.

--- src/sample/main.lua
+++ src/sample/main.lua
@@ -1,13 +1,25 @@
-local lib = require("win")
+local lib = require("sample.lib")
 
 local N = 2
 local MAXGEN = 10000
 local NOWORD = "\n"
 
+-- construct table
 local w1, w2 = NOWORD, NOWORD
 for w in lib.allwords() do
-  lib.insert(lib.prefix(w1, w2) w)
+  lib.insert(lib.prefix(w1, w2), w)
   w1 = w2; w2 = w
 end
 lib.insert(prefix(w1, w2), NOWORD)
 
+-- generate text
+w1 = NOWORD; w2 = NOWORD    --re-init
+for i=1, MAXGEN do
+  local list = lib.statetab[lib.prefix(w1, w2)]
+  -- random choice
+  local r = math.random(#list)
+  local nextword = list[r]
+  if nextword == NOWORD then return end
+  io.write(nextword, " ")
+  w1 = w2; w2 = NOWORD
+end
