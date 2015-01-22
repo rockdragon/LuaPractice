c586a9ac92c5364112b2b870fdd1b1adcfdd7627
unknown <ye.mo@BJPD007109.zhaopin.com.cn>
2015-01-22 15:26:18 +0800

    metatable exercise v2.

--- src/meta/main.lua
+++ src/meta/main.lua
@@ -1,7 +1,12 @@
 local Set = require("meta.set")
 
 local s1 = Set.new{10, 20, 30, 50}
-local s2 = Set.new{2, 3, 6}
+local s2 = Set.new{2, 3, 6, 10}
 
-print(getmetatable(s1))
-print(getmetatable(s2))
+--并集
+local s3 = s1 + s2
+Set.print(s3)
+
+--交集
+local s4 = s1 * s2
+Set.print(s4)
