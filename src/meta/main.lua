a9358a389543aec1ffb6391e926bef4d6933c42e
unknown <ye.mo@BJPD007109.zhaopin.com.cn>
2015-01-22 14:59:41 +0800

    metatable research.

---
+++ src/meta/main.lua
@@ -0,0 +1,4 @@
+local t = {}
+local t1 = {}
+setmetatable(t, t1)
+print(getmetatable(t))
--- src/storage/main.lua
+++ src/storage/main.lua
@@ -19,8 +19,7 @@ local s = "hello,2b,www"
 print(quote(s))
 --]]
 
-
-
+--[[
 function serialize(o)
   if type(o) == "number" then
     io.write(o)
@@ -44,3 +43,41 @@ local f = io.open("storage\\fdata", "a+")
 io.output(f)
 serialize(t)
 f:close()
+--]]
+
+
+function basicSerialize(o)
+  if type(o) == "number" then
+    return tostring(o)
+  else
+    return string.format("%q", o)
+  end  
+end
+
+function save(name, value, saved)
+  saved = saved or {}
+  io.write(name, "=")
+  if type(value) == "number" or type(lvalue) == "string" then
+    io.write(basicSerialize(value), "\n")
+  elseif type(value) == "table" then
+    if saved[value] then
+      io.write(saved[value], "\n")
+    else
+      saved[value] = name
+      io.write("{}\n")
+      for k, v in pairs(value) do
+        k = basicSerialize(k)
+        local name = string.format("%s[%s]", name, k)
+        save(fname, v, saved)
+      end      
+    end
+  else
+    error("cannot save a " .. type(value))
+  end 
+end
+
+local a = { x = 1, y = 2; {3, 4, 5}}
+a[2] = a
+a.z = a[1]
+
+save("a", a)
