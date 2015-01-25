0b00fd5f6a0b5ce9430e1e3c5fe06b58dcf05eca
Locke <moyerock@gmail.com>
2015-01-25 16:00:11 +0800

    self table technical provides class-level privacy.

---
+++ src/OO/selftable.lua
@@ -0,0 +1,22 @@
+function newAccount(initialBalance)
+  local self = {balance = initialBalance}
+  
+  local withdraw = function(v)
+    self.balance = self.balance - v
+  end
+  local deposit = function(v)
+    self.balance = self.balance + v
+  end
+  local getBalance = function()
+    return self.balance
+  end
+  
+  return {
+    withdraw = withdraw,
+    deposit = deposit,
+    getBalance = getBalance
+    }
+end
+
+local account = newAccount(1000)
+print(account.getBalance())
