4ace6894a36544e9160f73fd827223576c28599f
rockdragon <moyerock@gmail.com>
2015-01-27 17:06:14 +0800

    pine tree v2 :)

--- src/strings/main.lua
+++ src/strings/main.lua
@@ -1,13 +1,22 @@
-
-local str = "Hello."
-print(str:upper())
-
-local count = 19
-
-for i = 1, 10 do  
-  local num = 1 + (i-1) * 2
-  local space_num = count - i + 1
-  io.write(string.rep(" ", space_num)) 
-  io.write(string.rep("*", num))  
-  io.write("\n")
+function print_pine(count, lines)
+  return function()
+    for i = 1, lines do  
+      local num = 1 + (i-1) * 2
+      local space_num = count - i + 1
+      io.write(string.rep(" ", space_num)) 
+      io.write(string.rep("*", num))  
+      io.write("\n")
+    end
+  end
 end
+
+--[[
+      *
+     ***
+    *****
+   *******
+  *********
+ ***********
+--]]
+local darw = print_pine (21, 20)
+darw()
