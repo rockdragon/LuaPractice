87570f812544ebdc01713f4afc16a76be4df5732
rockdragon <moyerock@gmail.com>
2015-01-28 11:06:43 +0800

    html href&title extractor v2.

--- src/strings/main.lua
+++ src/strings/main.lua
@@ -1,6 +1,10 @@
 local LinkExtractor = require("strings.linkExtractor")
 
-local html = "<a href=\"http://book.douban.com/subject/20536784/\">The Shape of the New</a><a href=\"http://www.douban.com/\">台北</a>"
+local html = [[
+<a href="http://book.douban.com/subject/20536784/">The Shape of the New</a>
+<a href="http://www.douban.com/">台北</a>
+]]
+
 local extractor = LinkExtractor.extract(html)
 
 for link, title in extractor() do
