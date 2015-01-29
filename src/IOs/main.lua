ccaf46bf1d061dac7095730808904020da4225a5
Locke <moyerock@gmail.com>
2015-01-29 21:52:05 +0800

    word count.

--- src/IOs/main.lua
+++ src/IOs/main.lua
@@ -1,6 +1,15 @@
+--[[
 local no = 1
-for line in io.open("IOs\\encoding.lua"):lines() do
+for line in io.open("IOs/encoding.lua"):lines() do
   print(string.format("%02d.", no), line)
   no = no + 1
 end
+--]]
 
+local BUFSIZE = 128
+local lines, rest 
+local f = io.open("IOs/encoding.lua")
+repeat
+  lines, rest = f:read(BUFSIZE, "*l")
+  print(lines)
+until rest == nil
