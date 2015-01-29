4670d5b620474c36f950d048c1107cc1a5e6b1e4
rockdragon <moyerock@gmail.com>
2015-01-29 18:15:59 +0800

    io exercise.

--- src/IOs/main.lua
+++ src/IOs/main.lua
@@ -1,21 +1,6 @@
--- encoding to hex
-function encode(s)
-  return string.gsub(s, "([\128-\255=])", function(c)
-      return string.format("=%02X", string.byte(c))
-  end)
+local no = 1
+for line in io.open("IOs\\encoding.lua"):lines() do
+  print(string.format("%02d.", no), line)
+  no = no + 1
 end
 
---decoding from hex
-function decode(s)
-  return string.gsub(s, "=(%x%x)", function(c)      
-      return string.char(tonumber(c, 16))
-    end)
-end
-
-print("please input:")
-local t = io.read()
-t = encode(t)
-print(t)
-
-print("recover:")
-print(decode(t))
