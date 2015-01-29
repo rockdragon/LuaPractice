6f1e964c3b9294241af48e9b29c6263aa2689b57
rockdragon <moyerock@gmail.com>
2015-01-29 18:26:52 +0800

    io exercise.

--- src/IOs/main.lua
+++ src/IOs/main.lua
@@ -4,3 +4,10 @@ for line in io.open("IOs\\encoding.lua"):lines() do
   no = no + 1
 end
 
+local BUFSIZE = 128
+local line, rest 
+repeat
+	line, rest= io.open("IOs\\encoding.lua"):read(BUFSIZE, "*line") 
+	print(line, rest)
+until rest == 0
+
