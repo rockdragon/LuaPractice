ca41f2889193e7bd2f4651ca6032bea06f5f34ad
rockdragon <moyerock@gmail.com>
2015-02-04 17:40:37 +0800

    C calling Lua function.

--- src/CAPI/main.c
+++ src/CAPI/main.c
@@ -8,6 +8,34 @@
 #include "lualib.h"   
 #include "lauxlib.h"   
 
+int main(void){
+	int x = 10, y = 22, result = 0;
+
+	lua_State *L = luaL_newstate();
+	luaL_openlibs(L);							// 加载Lua库
+
+	if(luaL_loadfile(L, "func.lua") != 0)    	// load lua file
+		return 0;
+
+	lua_pcall(L, 0, LUA_MULTRET, 0);			//执行匿名函数，以编译源代码成二进制码
+                                            	//并将全局变量压栈（函数名也是变量）
+	lua_getglobal(L, "add");
+	lua_pushnumber(L, x);
+	lua_pushnumber(L, y);
+
+ 	if(lua_pcall(L, 2, 1, 0) != 0) {           // execute funciton
+        printf("\nError running function 'add' : %s", 
+			lua_tostring(L, -1));
+        return 0;
+    }
+
+	result = lua_tonumber(L, -1);
+	lua_pop(L, 1);
+
+	printf("Result: %d\n", result);
+}
+
+/*
 static void stackDump(lua_State *L){
 	int i;
 	int top = lua_gettop(L);	
@@ -36,9 +64,6 @@ static void stackDump(lua_State *L){
 	printf("\n");
 }
 
-/*
-	Lua interpreter
-*/
 int main(void){
 	char buff[256];
 	int error;
@@ -65,5 +90,4 @@ int main(void){
 	lua_close(L);
 	return 0;
 }
-
-
+*/
