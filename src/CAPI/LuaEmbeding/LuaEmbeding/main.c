a3cc9268f99e20f6140006745f3bce62594f8e56
Locke <moyerock@gmail.com>
2015-02-06 21:50:18 +0800

    three ways to call a function.

--- src/CAPI/LuaEmbeding/LuaEmbeding/main.c
+++ src/CAPI/LuaEmbeding/LuaEmbeding/main.c
@@ -6,6 +6,24 @@
 #include "lualib.h"
 #include "lauxlib.h"
 
+#define MAX_COLOR   255
+
+void error(lua_State *L, const char *msg){
+    printf("Error running in stack: %s\n",
+           lua_tostring(L, -1));
+}
+
+int get_field(lua_State *L, const char *key){
+    int result;
+    lua_pushstring(L, key);
+    lua_gettable(L, -2);       // retrieve table[key]
+    if(!lua_isnumber(L, -1))
+        error(L, "invalid component in background color.");
+    result = (int)lua_tonumber(L, -1) * MAX_COLOR;
+    lua_pop(L, 1);
+    return result;
+}
+
 int main(int argc, const char* argv[]){
     printf("loading lua.\n");
     
@@ -18,15 +36,25 @@ int main(int argc, const char* argv[]){
         return 0;
     
     lua_pcall(L, 0, LUA_MULTRET, 0);			//执行匿名函数，以编译源代码成二进制码
-    //并将全局变量压栈（函数名也是变量）
+    
+    // 第一种方式
+//并将全局变量压栈（函数名也是变量）
+//    lua_getglobal(L, "Func");
+//    lua_getfield(L, -1, "add");					//定位到函数
+    
+    //第二种方式
+//    lua_getglobal(L, "Func");
+//    lua_pushstring(L, "add");
+//    lua_gettable(L, -2);                          //定位到函数
+    
+    //第三种方式
     lua_getglobal(L, "Func");
-    lua_getfield(L, -1, "add");					//定位到函数
+    lua_getfield(L, -1, "add");                     //定位到函数
     lua_pushnumber(L, x);
     lua_pushnumber(L, y);
     
     if(lua_pcall(L, 2, 1, 0) != 0) {           // execute funciton
-        printf("\nError running function 'add' : %s",
-               lua_tostring(L, -1));
+        error(L, lua_tostring(L, -1));
         return 0;
     }
     
@@ -43,55 +71,3 @@ int main(int argc, const char* argv[]){
     printf("now stack top is: %d\n", lua_gettop(L));
 }
 
-/*
- static void stackDump(lua_State *L){
-	int i;
-	int top = lua_gettop(L);
-	for(i = 1; i <= top; i++){
- int t = lua_type(L, i);
- switch(t){
- case LUA_TSTRING: {
- printf("'%s'", lua_tostring(L, i));
- break;
- }
- case LUA_TBOOLEAN: {
- printf(lua_toboolean(L, i) ? "true" : "false");
- break;
- }
- case LUA_TNUMBER: {
- printf("%g", lua_tonumber(L, i));
- break;
- }
- default: {
- printf("%s", lua_typename(L, t));
- break;
- }
- }
- printf(" ");
-	}
-	printf("\n");
- }
- int main(void){
-	char buff[256];
-	int error;
-	lua_State *L = luaL_newstate();
-	luaL_openlibs(L);
-	
-	//ensure that there are at least 20 free stack
-	lua_checkstack(L, 20);
-	lua_pushstring(L, "hello");
-	stackDump(L);
-	lua_remove(L, 0);
-	stackDump(L);
-	while(fgets(buff, sizeof(buff), stdin) != NULL){
- error = luaL_loadbuffer(L, buff, strlen(buff), "line") ||
- lua_pcall(L, 0, 0, 0);
- if(error){
- fprintf(stderr, "%s", lua_tostring(L, -1));
- lua_pop(L, 1);
- }
-	}
-	lua_close(L);
-	return 0;
- }
- */
