4c993f814b243d0e0a54e77c9cbd5f2f0ee7ebe8
rockdragon <moyerock@gmail.com>
2015-02-10 18:13:12 +0800

    Luajit ffi calling sleep.

--- src/JIT/main.lua
+++ src/JIT/main.lua
@@ -1,5 +1,23 @@
 local ffi = require("ffi")
 ffi.cdef[[
+void Sleep(int ms);
+int poll(struct pollfd *fds, unsigned long nfds, int timeout);
 int printf(const char  *fmt, ...);
 ]]
 ffi.C.printf("hell luajit!\n")
+
+local sleep
+if ffi.os == "Windows" then
+  function sleep(s)
+    ffi.C.Sleep(s*1000)
+  end
+else
+  function sleep(s)
+    ffi.C.poll(nil, 0, s*1000)
+  end
+end
+
+for i=1, 100 do
+  sleep(0.1)
+  ffi.C.printf("\07")
+end
